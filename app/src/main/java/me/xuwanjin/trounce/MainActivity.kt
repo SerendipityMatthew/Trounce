3133a3061b1abc8ab3dc99a74df6163a200201bf
Matthew <xuwanjin1991@Gmail.com>
2020-10-27 18:20:07 +0800

    first commit for trounce
    Signed-off-by: Matthew <xuwanjin1991@Gmail.com>

---
+++ app/src/main/java/me/xuwanjin/trounce/MainActivity.kt
@@ -0,0 +1,38 @@
+package me.xuwanjin.trounce
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import me.xuwanjin.trounce.ui.TrounceTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TrounceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TrounceTheme {
+        Greeting("Android")
+    }
+}
